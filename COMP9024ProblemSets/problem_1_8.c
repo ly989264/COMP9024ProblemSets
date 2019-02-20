a01c670d901139d472eaa43e4a2a515f3d4d9a0f
ly989264 <ly989264@163.com>
2019-02-20 20:05:28 +1100

    fix bug

--- COMP9024ProblemSets/problem_1_8.c
+++ COMP9024ProblemSets/problem_1_8.c
@@ -8,10 +8,17 @@ void sort_sequence()
 	int temp;
 	while (scanf_s("%d",&temp))
 	{
-		if (cnt==size)
+		if (cnt==size-1)
 		{
 			size *= 2;
-			realloc(p, size);
+			// realloc(p, size*sizeof(int));
+			int *new_space = malloc(size * sizeof(int));
+			int *temp_space = p;
+			p = new_space;
+			new_space = temp_space;
+			for (unsigned i = 0; i < cnt; i++) *(p + i) = *(new_space + i);
+			free(new_space);
+			//Why when I use the realloc function, it will cause the error _crtisvalidheappointer(block)?
 		}
 		for (unsigned i=0;i<cnt;i++)
 		{
@@ -24,6 +31,9 @@ void sort_sequence()
 		}
 		*(p + cnt) = temp;
 		cnt++;
+		//cout the current array
+		for (int i = 0; i < cnt; i++) printf("%d ", *(p + i));
+		printf("\n");
 	}
 	for (unsigned j = 0; j < cnt; j++) printf("%d ", *(p + j));
 	printf("\n");
