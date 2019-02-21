5a1f9ac34ff9f47278a10a454416910f371c2fd4
ly989264 <ly989264@163.com>
2019-02-22 05:14:58 +1100

    add 2.3

---
+++ COMP9024ProblemSets/problem_2_3.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// #include "problem_2_1.h"
+// #include <string.h>
+//
+// int main(int argc, char**argv)
+// {
+// 	struct stack* x = initStack();
+// 	for (int i = 1;i<argc;i++)
+// 	{
+// 		int temp = atoi(argv[i]);
+// 		push(x, temp);
+// 	}
+// 	while(!is_empty(x))
+// 	{
+// 		printf("%d\n", pop(x));
+// 	}
+// 	return 0;
+// }
+
+//this is the main function version, so have to be commented
